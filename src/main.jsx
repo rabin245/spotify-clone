6ce56c801ae685f129a4182c5e26cedd36e24881
rabin245 <drarabin@gmail.com>
2023-05-11 12:57:36 +0545

    protect user specific routes

--- src/main.jsx
+++ src/main.jsx
@@ -9,6 +9,7 @@ import Login from "./pages/Login.jsx";
 import Signup from "./pages/Signup.jsx";
 import { RecoilRoot } from "recoil";
 import ProtectedAuthRoute from "./routes/ProtectedAuthRoute.jsx";
+import NeedAuthRoute from "./routes/NeedAuthRoute.jsx";
 
 const routes = createBrowserRouter([
   {
@@ -19,6 +20,30 @@ const routes = createBrowserRouter([
       {
         path: "/",
         element: <Home />,
+      },
+      {
+        path: "/library",
+        element: (
+          <NeedAuthRoute>
+            <div>Library</div>
+          </NeedAuthRoute>
+        ),
+      },
+      {
+        path: "/playlist/new",
+        element: (
+          <NeedAuthRoute>
+            <div>New Playlist</div>
+          </NeedAuthRoute>
+        ),
+      },
+      {
+        path: "/liked",
+        element: (
+          <NeedAuthRoute>
+            <div>Liked</div>
+          </NeedAuthRoute>
+        ),
       },
     ],
   },
