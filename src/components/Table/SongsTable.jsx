2ad9eae3d79a1756ecd21622639fbed53a205ef8
rabin245 <drarabin@gmail.com>
2023-05-14 15:19:38 +0545

    refactor playlist page components

---
+++ src/components/Table/SongsTable.jsx
@@ -0,0 +1,54 @@
+import { Container, Table } from "reactstrap";
+import { BsStopwatch } from "react-icons/bs";
+import TableSongCard from "../Card/TableSongCard";
+
+const SongsTable = ({ songsList, type = "playlist" }) => {
+  return (
+    <Container fluid className="p-4 m-0">
+      <Table responsive={true} borderless={true}>
+        <thead>
+          <tr
+            className="border-secondary"
+            style={{
+              color: "#797B7C",
+              borderBottom: "solid 1px #393C3E",
+            }}
+          >
+            <th>
+              <div
+                style={{
+                  marginLeft: "0.8rem",
+                }}
+              >
+                #
+              </div>
+            </th>
+            <th>Title</th>
+            <th>Album</th>
+            <th>Date Added</th>
+            <th
+              style={{
+                textAlign: "right",
+              }}
+            >
+              <div
+                style={{
+                  marginRight: "2rem",
+                }}
+              >
+                <BsStopwatch size={22} />
+              </div>
+            </th>
+          </tr>
+        </thead>
+        <tbody>
+          {songsList.map((songid, index) => (
+            <TableSongCard key={songid} songid={songid} index={index + 1} />
+          ))}
+        </tbody>
+      </Table>
+    </Container>
+  );
+};
+
+export default SongsTable;
--- src/pages/Playlist.jsx
+++ src/pages/Playlist.jsx
@@ -1,12 +1,8 @@
 import { useRecoilValue } from "recoil";
 import { playlistState } from "../recoilState";
 import { useParams } from "react-router-dom";
-import { Col, Container, Row, Table } from "reactstrap";
-import SpotifyIcon from "../assets/svg/spotify.svg";
-import PlayButton from "../components/Button/PlayButton";
-import { AiOutlineHeart } from "react-icons/ai";
-import { BsStopwatch } from "react-icons/bs";
-import TableSongCard from "../components/Card/TableSongCard";
+import Banner from "../components/Banner";
+import { SongsTable } from "../components/Table";
 
 const Playlist = () => {
   const { id } = useParams();
@@ -14,117 +10,15 @@ const Playlist = () => {
 
   return (
     <div className="playlistpage">
-      <div className="playlistpage--banner">
-        <div>
-          <Row className="p-4 m-0">
-            <Col
-              xs={12}
-              md={12}
-              lg={6}
-              xl={"auto"}
-              className="playlistpage--banner--image d-flex justify-content-center justify-content-md-center"
-            >
-              <img
-                src={playlist.image_url}
-                className="img-fluid rounded-3 shadow"
-                alt="..."
-              />
-            </Col>
-            <Col
-              xs={12}
-              md={12}
-              lg={6}
-              xl={"auto"}
-              className="playlistpage--banner-info d-flex flex-column justify-content-between text-white"
-            >
-              <h6 className="fw-bold mt-5">Playlist</h6>
-              <h1
-                className="fw-bold"
-                style={{
-                  fontSize: "6rem",
-                }}
-              >
-                {playlist.name}
-              </h1>
-              <p
-                className=""
-                style={{
-                  color: "#B3B9BF",
-                }}
-              >
-                {playlist.description}
-              </p>
-              <div className="d-flex align-items-center">
-                <span className="d-flex align-items-center">
-                  <img
-                    src={SpotifyIcon}
-                    alt="..."
-                    className="playlistpage--banner-spotifyicon me-2"
-                  />
-                  Spotify
-                </span>
-                <span className="mx-2">&bull;</span>
-                <span>{playlist.songs.length} songs</span>
-                <span>, about 2 hr</span>
-              </div>
-            </Col>
-          </Row>
+      <Banner
+        image_url={playlist.image_url}
+        name={playlist.name}
+        description={playlist.description}
+        numberOfSongs={playlist.songs.length}
+        type="playlist"
+      />
 
-          <div className="playlistpage--banner-buttons d-flex align-items-center py-4 px-4">
-            <div className="playlistpage--banner-button me-4">
-              <PlayButton size={35} />
-            </div>
-            <div className="playlistpage--banner-button">
-              <AiOutlineHeart size={35} />
-            </div>
-          </div>
-        </div>
-      </div>
-
-      <Container fluid className="p-4 m-0">
-        <Table responsive={true} borderless={true}>
-          <thead>
-            <tr
-              className="border-secondary"
-              style={{
-                color: "#797B7C",
-                borderBottom: "solid 1px #393C3E",
-              }}
-            >
-              <th>
-                <div
-                  style={{
-                    marginLeft: "0.8rem",
-                  }}
-                >
-                  #
-                </div>
-              </th>
-              <th>Title</th>
-              <th>Album</th>
-              <th>Date Added</th>
-              <th
-                style={{
-                  textAlign: "right",
-                }}
-              >
-                <div
-                  style={{
-                    marginRight: "2rem",
-                  }}
-                >
-                  <BsStopwatch size={22} />
-                </div>
-              </th>
-            </tr>
-          </thead>
-          <tbody>
-            {playlist.songs.map((songid, index) => (
-              <TableSongCard key={songid} songid={songid} index={index + 1} />
-            ))}
-          </tbody>
-        </Table>
-      </Container>
+      <SongsTable songsList={playlist.songs} type="playlist" />
     </div>
   );
 };
