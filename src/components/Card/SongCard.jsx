1f2b29d5d53407d39218477d171d2c0a6a016b67
rabin245 <drarabin@gmail.com>
2023-05-15 15:17:53 +0545

    make songs card resonsive for smaller screens

--- src/components/Card/SongCard.jsx
+++ src/components/Card/SongCard.jsx
@@ -4,10 +4,11 @@ import { BsPlayFill } from "react-icons/bs";
 const SongCard = ({ image_url, name, artist, duration, playSong }) => {
   return (
     <>
-      <div className="search--songs-card d-flex align-items-center p-1 rounded-2">
-        <div className="d-flex align-items-center me-auto">
+      <div className="search--songs-card d-flex justify-content-between align-items-center p-1 rounded-2">
+        <div className="d-flex align-items-center">
           <div className="search--songs-card-img-wrapper">
             <img src={image_url} style={{ width: "40px", height: "40px" }} />
+
             <div className="search--songs-card-img-overlay" onClick={playSong}>
               <BsPlayFill
                 size={20}
@@ -16,19 +17,23 @@ const SongCard = ({ image_url, name, artist, duration, playSong }) => {
               />
             </div>
           </div>
+
           <div className="ms-2 d-flex flex-column">
             <span className="p-0 m-0 fw-semibold">{name}</span>
             <span style={{ fontSize: "0.9rem" }}>{artist}</span>
           </div>
         </div>
+
         <div className="d-flex align-items-center me-3">
-          <span className="me-5">
+          <span className="me-1 me-sm-5">
             <AiOutlineHeart
               size={20}
               className="search--songs-card-hidden-icon"
             />
           </span>
-          <span className="me-2">{duration}</span>
+
+          <span className="me-1 me-sm-2">{duration}</span>
+
           <span>
             <AiOutlineEllipsis
               size={20}
