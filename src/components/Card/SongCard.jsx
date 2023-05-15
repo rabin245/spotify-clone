d0b95007e440e081c5d2fdf0de3155521060bc71
rabin245 <drarabin@gmail.com>
2023-05-15 11:34:36 +0545

    add play icon on song card hover

--- src/components/Card/SongCard.jsx
+++ src/components/Card/SongCard.jsx
@@ -1,11 +1,21 @@
 import { AiOutlineEllipsis, AiOutlineHeart } from "react-icons/ai";
+import { BsPlayFill } from "react-icons/bs";
 
 const SongCard = ({ image_url, name, artist, duration }) => {
   return (
     <>
       <div className="search--songs-card d-flex align-items-center p-1 rounded-2">
         <div className="d-flex align-items-center me-auto">
-          <img src={image_url} style={{ width: "40px", height: "40px" }} />
+          <div className="search--songs-card-img-wrapper">
+            <img src={image_url} style={{ width: "40px", height: "40px" }} />
+            <div className="search--songs-card-img-overlay">
+              <BsPlayFill
+                size={20}
+                fill="white"
+                className="search--songs-card-img-play"
+              />
+            </div>
+          </div>
           <div className="ms-2 d-flex flex-column">
             <span className="p-0 m-0 fw-semibold">{name}</span>
             <span style={{ fontSize: "0.9rem" }}>{artist}</span>
